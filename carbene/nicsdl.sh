3518cf8775b38a2a5bdad78f7ba06eaa4d7ef041
Jonathan Yong <yongrenjie@gmail.com>
2020-01-10 12:30:52 +0000

    reset target to yongrenjie

--- carbene/nicsdl.sh
+++ carbene/nicsdl.sh
@@ -1,14 +1,13 @@
 dl_nics () {
     mkdir -p ~/nicspy
-    wget https://raw.githubusercontent.com/duartegroup/sbmcc/master/carbene/nics1.py -O ~/nicspy/nics1.py
-    wget https://raw.githubusercontent.com/duartegroup/sbmcc/master/carbene/nics2.py -O ~/nicspy/nics2.py
-    wget https://raw.githubusercontent.com/duartegroup/sbmcc/master/carbene/nics1 -O ~/nicspy/nics1
-    wget https://raw.githubusercontent.com/duartegroup/sbmcc/master/carbene/nics2 -O ~/nicspy/nics2
+    wget https://raw.githubusercontent.com/yongrenjie/sbmcc/master/carbene/nics1.py -O ~/nicspy/nics1.py
+    wget https://raw.githubusercontent.com/yongrenjie/sbmcc/master/carbene/nics2.py -O ~/nicspy/nics2.py
+    wget https://raw.githubusercontent.com/yongrenjie/sbmcc/master/carbene/nics1 -O ~/nicspy/nics1
+    wget https://raw.githubusercontent.com/yongrenjie/sbmcc/master/carbene/nics2 -O ~/nicspy/nics2
     chmod +x ~/nicspy/nics1
     chmod +x ~/nicspy/nics2
     echo 'export PATH=$HOME/nicspy:$PATH' >> ~/.bash_profile
     source ~/.bash_profile
-    fi
 }
 
 dl_nics
